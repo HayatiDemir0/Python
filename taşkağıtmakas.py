22c109ac36c693fc2fcf15454b23533cc992ef74
Hayati Demir <hayatidemiirr@gmail.com>
2025-04-26 22:02:14 +0300

    py

---
+++ taşkağıtmakas.py
@@ -0,0 +1,49 @@
+import random
+
+tas = '''
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+kagit = '''
+   _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+makas = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+oyun_gorselleri = [tas, kagit, makas]
+
+kullanici_secimi = int(input("Ne seçiyorsun? 0 için Taş, 1 için Kağıt, 2 için Makas yaz.\n"))
+if kullanici_secimi >= 0 and kullanici_secimi <= 2:
+    print(oyun_gorselleri[kullanici_secimi])
+else:
+    print("Geçersiz bir sayı girdiniz. Kaybettiniz!")
+    exit()
+
+bilgisayar_secimi = random.randint(0, 2)
+print("Bilgisayar seçti:")
+print(oyun_gorselleri[bilgisayar_secimi])
+
+if bilgisayar_secimi == 0 and kullanici_secimi == 2:
+    print("Kaybettin!")
+elif kullanici_secimi == 0 and bilgisayar_secimi == 2:
+    print("Kazandın!")
+elif bilgisayar_secimi > kullanici_secimi:
+    print("Kaybettin!")
+elif kullanici_secimi > bilgisayar_secimi:
+    print("Kazandın!")
+elif bilgisayar_secimi == kullanici_secimi:
+    print("Berabere!")
