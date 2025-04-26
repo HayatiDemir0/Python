396ecf34114dc15f1c7101c064d50d225d1708c4
Hayati Demir <hayatidemiirr@gmail.com>
2025-04-26 22:04:43 +0300

    py

--- taşkağıtmakas.py
+++ taşkağıtmakas.py
@@ -29,18 +29,17 @@ oyun_gorselleri = [tas, kagit, makas]
 kullanici_secimi = int(input("Ne seçiyorsun? 0 için Taş, 1 için Kağıt, 2 için Makas yaz.\n"))
 if kullanici_secimi >= 0 and kullanici_secimi <= 2:
     print(oyun_gorselleri[kullanici_secimi])
-else:
-    print("Geçersiz bir sayı girdiniz. Kaybettiniz!")
-    exit()
 
 bilgisayar_secimi = random.randint(0, 2)
-print("Bilgisayar seçti:")
+print(f"Bilgisayar seçti:")
 print(oyun_gorselleri[bilgisayar_secimi])
 
 if bilgisayar_secimi == 0 and kullanici_secimi == 2:
     print("Kaybettin!")
 elif kullanici_secimi == 0 and bilgisayar_secimi == 2:
     print("Kazandın!")
+elif kullanici_secimi >= 3 or kullanici_secimi < 0:
+    print("Geçersiz bir sayı girdin. Kaybettin!")
 elif bilgisayar_secimi > kullanici_secimi:
     print("Kaybettin!")
 elif kullanici_secimi > bilgisayar_secimi:
